51aac6372636a2c0be3fbf4f36847642af9ee1f3
Munifa Ahmed <munifanasser@gmail.com>
2018-11-07 16:05:36 -0800

    Updates

---
+++ precache-manifest.ce0ac657bdb9cba6f7950ebfb45cc645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d936849632184e0df2f701d4cd47001",
+    "url": "/static/media/photo1.6d936849.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "261d1b1a467a23087c1e",
+    "url": "/static/js/main.261d1b1a.chunk.js"
+  },
+  {
+    "revision": "fdf81112d21900793b36",
+    "url": "/static/js/1.fdf81112.chunk.js"
+  },
+  {
+    "revision": "261d1b1a467a23087c1e",
+    "url": "/static/css/main.d18835e3.chunk.css"
+  },
+  {
+    "revision": "79e94a13501bdc497d64cb15d287c3ad",
+    "url": "/index.html"
+  }
+];
